701cafc0954feef33f8a31e0a78d09f516a8b236
sushma8919 <162419003+sushma8919@users.noreply.github.com>
2025-11-26 22:21:17 +0530

    Create Stud class with greeting messages
    Added a Java class that prints greeting messages.

---
+++ Stud.java
@@ -0,0 +1,17 @@
+public class Stud
+{
+public static void main(String args[])
+{
+System.out.println("hello,world");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+System.out.println("Hello,good morning");
+}
+}
